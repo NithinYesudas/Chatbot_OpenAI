82c87ed2536c04ab4bdbd3c6a81abb4ed1ae0250
NithinYesudas <nithinypadickal@gmail.com>
2023-12-28 22:35:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import openai
+app = Flask(__name__)
+openai.api_key  = "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"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response['choices'][0]['message']['content']
+@app.route("/")
+def home():    
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')  
+    response = get_completion(userText)  
+    #return str(bot.get_response(userText)) 
+    return response
